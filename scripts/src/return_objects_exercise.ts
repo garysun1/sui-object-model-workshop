ebc2402a5f5698be9883bdc55b5ae7600dfb1ea9
Gary Sun <garysun08@gmail.com>
2026-03-14 14:22:54 -0700

    completed workshop

--- scripts/src/return_objects_exercise.ts
+++ scripts/src/return_objects_exercise.ts
@@ -1,14 +1,7 @@
 import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
 import keyPairJson from "../keypair.json" with { type: "json" };
 import { SuiGrpcClient } from "@mysten/sui/grpc";
-
-/**
- *
- * Global variables
- *
- * These variables are used throughout the exercise below.
- *
- */
+import { Transaction } from "@mysten/sui/transactions";
 const keypair = Ed25519Keypair.fromSecretKey(keyPairJson.privateKey);
 const suiAddress = keypair.getPublicKey().toSuiAddress();
 
@@ -19,65 +12,21 @@ const suiClient = new SuiGrpcClient({
 	baseUrl: 'https://fullnode.testnet.sui.io:443',
 });
 
-/**
- * Returning Objects: Exercise 1
- *
- * In this exercise, you will be returned a new object from a function and must transfer it to an
- * address, otherwise, the transaction will abort.
- *
- * When finished, run the following command in the scripts directory to test your solution:
- *
- * pnpm return-objects
- *
- * RESOURCES:
- * - https://sdk.mystenlabs.com/typescript/transaction-building/basics#transactions
- */
 const main = async () => {
-  /**
-   * Task 1:
-   *
-   * Create a new Transaction instance from the @mysten/sui/transactions module.
-   */
+  const tx = new Transaction();
 
-  /**
-   * Task 2:
-   *
-   * Execute the call to the `sui_nft::new` function to the transaction instance.
-   *
-   * The target should be in the format {package address}::{module name}::{function name}. The
-   * package address is provided above. The module name is `sui_nft` and the function name is `new`.
-   *
-   * HINT: The arguments and typeArguments arguments are optional since this function does not take
-   * any arguments or type arguments.
-   */
+  const nft = tx.moveCall({
+    target: `${PACKAGE_ID}::sui_nft::new`,
+  });
 
-  /**
-   * Task 3:
-   *
-   * Transfer the newly created SuiNFT object to your address.
-   *
-   * Use `tx.transferObjects(objects, address)` - Transfers a list of objects to the specified address.
-   *
-   * HINT: Use `suiAddress`` to transfer the object to your address.
-   */
+  tx.transferObjects([nft], suiAddress);
 
+  const result = await suiClient.signAndExecuteTransaction({
+    transaction: tx,
+    signer: keypair,
+  });
 
-  /**
-   * Task 4:
-   *
-   * Sign and execute the transaction using the SuiClient instance created above.
-   *
-   * Print the result to the console.
-   */
-
-
-  /**
-   * Task 5: Run the script with the command below and ensure it works!
-   * 
-   * pnpm return-objects
-   * 
-   * Verify the transaction on the Sui Explorer: https://suiscan.xyz/testnet/home
-   */
+  console.log(result);
 };
 
 main();
--- scripts/src/scavenger_hunt_exercise.ts
+++ scripts/src/scavenger_hunt_exercise.ts
@@ -1,88 +1,46 @@
 import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
 import keyPairJson from "../keypair.json" with { type: "json" };
 import { SuiGrpcClient } from "@mysten/sui/grpc";
+import { Transaction } from "@mysten/sui/transactions";
 
-/**
- *
- * Global variables
- *
- * These variables are used throughout the exercise below.
- *
- */
 const keypair = Ed25519Keypair.fromSecretKey(keyPairJson.privateKey);
+const suiAddress = keypair.getPublicKey().toSuiAddress();
 
 const PACKAGE_ID = `0x9603a31f4b3f32843b819b8ed85a5dd3929bf1919c6693465ad7468f9788ef39`;
 const VAULT_ID = `0x8d85d37761d2a4e391c1b547c033eb0e22eb5b825820cbcc0c386b8ecb22be33`;
+const VAULT_CODE = 745223;
 
 const suiClient = new SuiGrpcClient({
 	network: 'testnet',
 	baseUrl: 'https://fullnode.testnet.sui.io:443',
 });
 
-/**
- * Scavenger Hunt: Exercise 3
- *
- * In this exercise, you use Sui objects as inputs in a PTB to update the value of a shared object.
- *
- * When finished, run the following command in the scripts directory to test your solution:
- *
- * pnpm scavenger-hunt
- *
- * RESOURCES:
- * - https://sdk.mystenlabs.com/typescript/transaction-building/basics#transactions
- */
 const main = async () => {
-  /**
-   * Task 1:
-   *
-   * Create a new Transaction instance from the @mysten/sui/transactions module.
-   */
+  const tx = new Transaction();
 
-  /**
-   * Task 2:
-   *
-   * Create a new key using the `key::new` function.
-   */
+  const key = tx.moveCall({
+    target: `${PACKAGE_ID}::key::new`,
+  });
 
-  /**
-   * Task 3:
-   *
-   * Set the key code correctly using the `key::set_code` function.
-   */
+  tx.moveCall({
+    target: `${PACKAGE_ID}::key::set_code`,
+    arguments: [key, tx.pure.u64(VAULT_CODE)],
+  });
 
-  /**
-   * Task 4:
-   *
-   * Use the key to withdraw the `SUI` coin from the vault using the `vault::withdraw` function.
-   */
-  
+  const coin = tx.moveCall({
+    target: `${PACKAGE_ID}::vault::withdraw`,
+    typeArguments: [`0x2::sui::SUI`],
+    arguments: [tx.object(VAULT_ID), key],
+  });
 
-  /**
-   * Task 5:
-   *
-   * Transfer the `SUI` coin to your account.
-   */
+  tx.transferObjects([coin], suiAddress);
 
+  const result = await suiClient.signAndExecuteTransaction({
+    transaction: tx,
+    signer: keypair,
+  });
 
-  /**
-   * Task 6:
-   *
-   * Sign and execute the transaction using the SuiClient instance created above.
-   *
-   * Print the result to the console.
-   *
-   * Resources:
-   * - Observing transaction results: https://sdk.mystenlabs.com/typescript/transaction-building/basics#observing-the-results-of-a-transaction
-   */
-
-
-  /**
-   * Task 7: Run the script with the command below and ensure it works!
-   * 
-   * pnpm scavenger-hunt
-   * 
-   * Verify the transaction on the Sui Explorer: https://suiscan.xyz/testnet/home
-   */
+  console.log(result);
 };
 
 main();
